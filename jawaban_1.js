d0521bfe3bf5225706acf13000398f135a13cad5
jlerapatty <lerapattyjoaquin@gmail.com>
2021-09-21 10:28:23 +0700

    Joaquin

--- jawaban_1.js
+++ jawaban_1.js
@@ -1,1 +1,10 @@
 // Jawaban Soal 1 ada di bawah ini
+const Animal = {
+    Name: "John",
+    Breed: "Bull Dog",
+    weight: 38,
+    soundDog: "aug aug",
+    sound : function() {
+      return this.sound;
+    }
+  };
